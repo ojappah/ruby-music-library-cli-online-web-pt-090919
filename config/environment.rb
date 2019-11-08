43af8df462b659bbe4f26116e671981583237849
generous-vector-9581 <ojappah@gmail.com>
2019-11-08 22:05:03 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,8 @@
 require 'bundler'
 Bundler.require
 
-# require_relative "../lib/song.rb"
-# require_relative "../lib/artist.rb"
-# require_relative "../lib/genre.rb"
-
 module Concerns
 end
+
+
+require_all 'lib'
